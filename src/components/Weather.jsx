43fdb58b025e2472847060fa4eac74c325976bbc
Ayush Kumar Gupta <ayush.skg@gmail.com>
2023-12-01 01:38:33 +0530

    changes

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -24,7 +24,7 @@ function Weather() {
     const getAllData = async () => {
       try {
         const response = await axios.get(
-          `http://api.openweathermap.org/data/2.5/forecast?zip=${id},In&appid=${APIKEY}&units=metric`
+          `https://api.openweathermap.org/data/2.5/forecast?zip=${id},In&appid=${APIKEY}&units=metric`
         );
         console.log(response.data);
         setWeatherData(response.data);
